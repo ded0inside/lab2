e7830552adbb830f79eba02fcbf6f9f4f4cbdfd5
Дулькин Илья <ilia.dulkin@gmail.com>
2020-12-25 20:08:08 +0300

    Update lab_2.py

--- lab_2.py
+++ lab_2.py
@@ -1,20 +1,18 @@
 import csv
 
 
-def get_text_value(message):
+def text_equalizer(message):
     while True:
-        value = input(message)
-        if value == '' or value.isalpha():
+        if message == '' or message.isalpha():
             break
-    return value.lower()
+    return message.lower()
 
 
-def get_num_value(message):
+def num_equalizer(message):
     while True:
-        value = input(message)
-        if value == '' or value.isdigit():
+        if message == '' or message.isdigit():
             break
-    return value.lower()
+    return message.lower()
 
 
 def check_year(user_year, game_year):
@@ -44,55 +42,51 @@ def check_price(user_price, game_price):
     return user_price == '' or game_price <= float(user_price)
 
 
-def check_rating(user_rating, game_rating):
-    return user_rating == '' or game_rating >= float(user_rating)
+def check_developer(user_developer, game_developer):
+    return (user_developer == [''] or
+            any(u_developer in game_developer for u_developer in user_developer))
 
 
-print('Вас приветсвтвует мастер поиска игр!')
-print('Пройдите краткий опрос, чтобы мы смогли посоветовать вам игры.')
-print('Вводите ответы через запятую. Чтобы пропустить - нажмите Enter.')
+message = input("Укажите максимальный год выпуска: ")
+year = num_equalizer(message)
 
-message = 'Какой максимальный год выпуска игры вас интересует? '
-years = get_num_value(message)
+message = input('Укажите интересующего Вас разработчика: ')
+developer = text_equalizer(message).split(',')
 
-message = 'Какой жанр игр вас интересует (Action, RPG)? '
-genres = get_text_value(message).split(',')
+message = input('Укажите платформу игры: ')
+platform = text_equalizer(message).split(',')
 
-message = 'Укажите минимальное ограничение по возрасту: '
-age = get_num_value(message)
+message = input('Укажите минимальное ограничение по возрасту: ')
+age = num_equalizer(message)
 
-message = 'Укажите категорию игры (Multiplayer, Online): '
-categories = get_text_value(message).split(',')
+message = input('Укажите категорию игры: ')
+categories = text_equalizer(message).split(',')
 
-message = 'На какую платформу планируется установка (windows, mac, linux)? '
-platforms = get_text_value(message).split(',')
+message = input('Укажите жанр игры: ')
+genres = text_equalizer(message).split(',')
 
-message = 'Укажите максимальную цену игры ($): '
-prices = get_num_value(message)
-
-message = 'Укажите минимальный процент положительных отзывов: '
-ratings = get_num_value(message)
-
+message = input('Укажите максимальную цену игры в доллорах: ')
+price = num_equalizer(message)
 
 with open('steam.csv', encoding='utf-8') as f, \
-        open('out.txt', 'w', encoding='utf-8') as out:
+        open('answer.txt', 'w', encoding='utf-8') as ans:
     reader = csv.reader(f)
     collumns = next(reader)
     for row in reader:
         game_name = row[1]
         game_year = int(row[2].lower().split('-')[0])
+        game_developer = row[4].lower().split(';')
         game_platform = row[6].lower().split(';')
         game_age = int(row[7])
         game_category = row[8].lower().split(';')
         game_genres = row[9].lower().split(';')
         game_price = float(row[17])
-        game_rating = float(int(row[12])/(int(row[12])+int(row[13])))*100
 
-        if (check_year(years, game_year)
+        if (check_year(year, game_year)
                 and check_genre(genres, game_genres)
                 and check_age(age, game_age)
                 and check_category(categories, game_category)
-                and check_platform(platforms, game_platform)
-                and check_price(prices, game_price)
-                and check_rating(ratings, game_rating)):
-            out.write(game_name + '\n')
+                and check_platform(platform, game_platform)
+                and check_price(price, game_price)
+                and check_developer(developer, game_developer)):
+            ans.write(game_name + '\n')
